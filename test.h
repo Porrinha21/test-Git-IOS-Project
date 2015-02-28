cd19540157ec973c44cf72dc7271665c7042ebe1
Pedro Luis <PedroLuis@MBP-de-Pedro.lan>
2015-02-28 17:46:32 +0000

    added a test class

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Gif test
+//
+//  Created by Pedro Luis on 28/02/15.
+//  Copyright (c) 2015 Pedro Luis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  Gif test
+//
+//  Created by Pedro Luis on 28/02/15.
+//  Copyright (c) 2015 Pedro Luis. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
